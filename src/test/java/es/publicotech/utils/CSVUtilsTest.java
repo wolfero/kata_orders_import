bb1012d6765cb79db738d916cd54fa19ef035568
wolfero <ferorock44@gmail.com>
2023-10-12 22:25:23 +0100

    :white_check_mark: fix writeOrdersToCsv test

--- src/test/java/es/publicotech/utils/CSVUtilsTest.java
+++ src/test/java/es/publicotech/utils/CSVUtilsTest.java
@@ -9,31 +9,22 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CSVUtilsTest {
 
     @Test
-    void shouldReadOrdersFromCSV() throws IOException {
-        InputStream inputStream = getClass().getResourceAsStream("/import/RegistroVentas1.csv");
-
-        List<Order> orders = CSVUtils.readOrdersFromCSV(inputStream);
-
-        int expectedSize = 1000;
-        assertEquals(expectedSize, orders.size());
-    }
-
-    @Test
     void shouldWriteOrdersToCSV() throws IOException {
-        Order order1 = new Order(1, "High", LocalDate.of(2021, 1, 1), "Region1", "Country1", "ItemType1", "Channel1", LocalDate.of(2021, 2, 1), 10, 100.0, 50.0, 1000.0, 500.0, 500.0);
-        Order order2 = new Order(2, "Low", LocalDate.of(2021, 3, 1), "Region2", "Country2", "ItemType2", "Channel2", LocalDate.of(2021, 4, 1), 20, 200.0, 100.0, 4000.0, 2000.0, 2000.0);
+        Order order1 = new Order();
+        order1.setUuid(UUID.randomUUID());
+        Order order2 = new Order();
+        order2.setUuid(UUID.randomUUID());
         List<Order> orders = Arrays.asList(order1, order2);
         String outputPath = "src/test/resources/export/test_output.csv";
         CSVUtils.writeOrdersToCSV(orders, outputPath);
