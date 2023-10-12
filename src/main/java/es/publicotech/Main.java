14a6ca9535b99e3d009aa5f444906cd42b2159ad
wolfero <ferorock44@gmail.com>
2023-10-12 17:11:18 +0100

    :zap: add business logic in main class

--- src/main/java/es/publicotech/Main.java
+++ src/main/java/es/publicotech/Main.java
@@ -1,7 +1,22 @@
 package es.publicotech;
 
+import es.publicotech.database.DataBaseConnector;
+import es.publicotech.repository.OrderRepository;
+import es.publicotech.services.OrderService;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final String importCsvPath = "/import/RegistroVentas1.csv"; //small
+    //    private static final String importCsvPath = "/import/RegistroVentas2.csv"; //big
+    private static final String exportCsvPath = "src/main/resource/export/ExportedOrders.csv";
+
+    public static void main(String[] args) throws SQLException, IOException {
+        DataBaseConnector dbConnector = new DataBaseConnector();
+        OrderRepository orderRepository = new OrderRepository(dbConnector);
+        OrderService orderService = new OrderService(orderRepository);
+        orderService.importOrdersFromCSVIntoDB(importCsvPath);
+        orderService.exportOrdersIntoCSV(exportCsvPath);
     }
 }
