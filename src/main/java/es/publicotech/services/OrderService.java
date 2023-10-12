066008254efc61adeda84a6d42c7cd76f30f377a
wolfero <ferorock44@gmail.com>
2023-10-12 22:15:45 +0100

    :sparkles: add importOrdersFromApiIntoDB in service

--- src/main/java/es/publicotech/services/OrderService.java
+++ src/main/java/es/publicotech/services/OrderService.java
@@ -1,6 +1,7 @@
 package es.publicotech.services;
 
-import es.publicotech.Main;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.publicotech.models.API.ApiOrders;
 import es.publicotech.models.Order;
 import es.publicotech.models.interfaces.OrderServiceInterface;
 import es.publicotech.repository.OrderRepository;
@@ -8,12 +9,15 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import static es.publicotech.utils.CSVUtils.readOrdersFromCSV;
 import static es.publicotech.utils.CSVUtils.writeOrdersToCSV;
 import static es.publicotech.utils.ConsoleUtils.resume;
 
@@ -23,12 +27,44 @@ public class OrderService implements OrderServiceInterface {
     private OrderRepository orderRepository;
 
     @Override
-    public void importOrdersFromCSVIntoDB(String importCsvPath) throws IOException, SQLException {
-        try (InputStream inputStream = Main.class.getResourceAsStream(importCsvPath)) {
-            List<Order> orders = readOrdersFromCSV(inputStream);
-            orderRepository.saveOrders(orders);
-            resume(orders);
-            log.info("Orders were successfully imported!");
+    public void importOrdersFromApiIntoDB(String url) throws IOException, SQLException {
+        ApiOrders apiOrders = getApiOrders(url);
+        List<Order> ordersToSave = new ArrayList<>();
+        long orderCount = 0;
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+//----------WARNING, THE API HAS MORE THAN HALF A MILLION RECORDS THAT WILL BE LOADED INTO THE DATABASE AND THEN INTO THE DUMP FILE---------//
+//----------------------------------------------------------------------------------------------------------------------------------------//
+        while (orderCount < 4) {
+            ordersToSave.addAll(apiOrders.getOrders());
+            String nextPage = apiOrders.getLinks().getNext();
+            apiOrders = getApiOrders(nextPage);
+            orderCount++;
+            log.info("Page " + orderCount + " of orders loaded!");
+        }
+        orderRepository.saveOrders(ordersToSave);
+        resume(ordersToSave);
+        log.info(orderCount + " Pages of orders were successfully imported!");
+    }
+
+    private ApiOrders getApiOrders(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Accept", "application/json")
+                .build();
+
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(OrderService::parseJson)
+                .join();
+    }
+
+    private static ApiOrders parseJson(String response) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(response, ApiOrders.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse JSON response", e);
         }
     }
 
