b2e13baa2a9bafe43b68dcc6577f50cc3c653801
wolfero <ferorock44@gmail.com>
2023-10-12 16:54:05 +0100

    :sparkles: add orders service

---
+++ src/main/java/es/publicotech/services/OrderService.java
@@ -0,0 +1,37 @@
+package es.publicotech.services;
+
+import es.publicotech.Main;
+import es.publicotech.models.Order;
+import es.publicotech.models.interfaces.OrderServiceInterface;
+import es.publicotech.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+import static es.publicotech.utils.CSVUtils.readOrdersFromCSV;
+import static es.publicotech.utils.ConsoleUtils.resume;
+
+@Slf4j
+@AllArgsConstructor
+public class OrderService implements OrderServiceInterface {
+    private OrderRepository orderRepository;
+
+    @Override
+    public void importOrdersFromCSVIntoDB(String importCsvPath) throws IOException, SQLException {
+        try (InputStream inputStream = Main.class.getResourceAsStream(importCsvPath)) {
+            List<Order> orders = readOrdersFromCSV(inputStream);
+            orderRepository.saveOrders(orders);
+            resume(orders);
+            log.info("Orders were successfully imported!");
+        }
+    }
+
+    @Override
+    public void exportOrdersIntoCSV(String exportCsvPath) throws SQLException, IOException {
+
+    }
+}
