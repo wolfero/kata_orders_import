fb7a2cc696cee38ad8961bdd075ebc6f9e609360
wolfero <ferorock44@gmail.com>
2023-10-12 16:57:24 +0100

    :sparkles: add exportOrdersIntoCSV in OrderService

--- src/main/java/es/publicotech/services/OrderService.java
+++ src/main/java/es/publicotech/services/OrderService.java
@@ -10,9 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.List;
 
 import static es.publicotech.utils.CSVUtils.readOrdersFromCSV;
+import static es.publicotech.utils.CSVUtils.writeOrdersToCSV;
 import static es.publicotech.utils.ConsoleUtils.resume;
 
 @Slf4j
@@ -32,6 +34,16 @@ public class OrderService implements OrderServiceInterface {
 
     @Override
     public void exportOrdersIntoCSV(String exportCsvPath) throws SQLException, IOException {
+        List<Order> orders = orderRepository.loadAllOrders();
+        if (orders != null) {
+            List<Order> sortedOrders = getOrdersSortedById(orders);
+            writeOrdersToCSV(sortedOrders, exportCsvPath);
+            log.info("Orders were successfully exported into " + exportCsvPath + " file!");
+        }
+    }
 
+    private List<Order> getOrdersSortedById(List<Order> orders) {
+        orders.sort(Comparator.comparing(Order::getOrderId));
+        return orders;
     }
 }
