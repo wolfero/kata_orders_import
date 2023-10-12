8f0d9cbda598941721705e5c5650965a5d940160
wolfero <ferorock44@gmail.com>
2023-10-12 22:26:50 +0100

    :white_check_mark: fix shouldExportOrdersIntoCSV test

--- src/test/java/es/publicotech/services/OrderServiceTest.java
+++ src/test/java/es/publicotech/services/OrderServiceTest.java
@@ -2,21 +2,16 @@ package es.publicotech.services;
 
 import es.publicotech.models.Order;
 import es.publicotech.repository.OrderRepository;
-import es.publicotech.utils.CSVUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
 public class OrderServiceTest {
@@ -42,9 +37,11 @@ public class OrderServiceTest {
     public void shouldExportOrdersIntoCSV() throws SQLException, IOException {
         String exportCsvPath = "src/test/resources/export/ExportedOrders.csv";
         List<Order> orders = new ArrayList<>();
-        Order order1 = new Order(1, "High", LocalDate.now(), "Region", "Country", "ItemType", "Channel", LocalDate.now(), 10, 10.0, 5.0, 100.0, 50.0, 50.0);
+        Order order1 = new Order();
+        order1.setId("1");
         orders.add(order1);
-        Order order2 = new Order(2, "High", LocalDate.now(), "Region", "Country", "ItemType", "Channel", LocalDate.now(), 10, 10.0, 5.0, 100.0, 50.0, 50.0);
+        Order order2 = new Order();
+        order2.setId("2");
         orders.add(order2);
         when(orderRepository.loadAllOrders()).thenReturn(orders);
 
