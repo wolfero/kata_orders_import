2f7fd25e9956da5c277fcb040d58945a9c3651a8
wolfero <ferorock44@gmail.com>
2023-10-12 22:08:14 +0100

    :card_file_box: update table from csv Orders to API Orders

--- init.sql
+++ init.sql
@@ -1,17 +1,22 @@
 CREATE SCHEMA IF NOT EXISTS ordersschema;
 CREATE TABLE ordersschema.orders (
-    order_id SERIAL PRIMARY KEY,
-    order_priority VARCHAR(255),
-    order_date DATE,
+    uuid UUID PRIMARY KEY,
+    id VARCHAR(255),
     region VARCHAR(255),
     country VARCHAR(255),
     item_type VARCHAR(255),
     sales_channel VARCHAR(255),
+    priority VARCHAR(255),
+    date DATE,
     ship_date DATE,
-    units_sold INT,
-    unit_price DECIMAL(15, 2),
-    unit_cost DECIMAL(15, 2),
-    total_revenue DECIMAL(15, 2),
-    total_cost DECIMAL(15, 2),
-    total_profit DECIMAL(15, 2)
+    units_sold BIGINT,
+    unit_price NUMERIC(10, 2),
+    unit_cost NUMERIC(10, 2),
+    total_revenue NUMERIC(15, 2),
+    total_cost NUMERIC(15, 2),
+    total_profit NUMERIC(15, 2)
+);
+CREATE TABLE ordersschema.links (
+    order_uuid UUID PRIMARY KEY,
+    self VARCHAR(1024)
 );
