605d4c8948bc39e3ec2e8b98668a405b8b0d5d75
wolfero <ferorock44@gmail.com>
2023-10-13 12:54:30 +0100

    :white_check_mark: fix failed tests

--- src/test/java/es/publicotech/repository/OrderRepositoryTest.java
+++ src/test/java/es/publicotech/repository/OrderRepositoryTest.java
@@ -2,63 +2,83 @@ package es.publicotech.repository;
 
 import es.publicotech.database.DataBaseConnector;
 import es.publicotech.models.Order;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.*;
-import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class OrderRepositoryTest {
-    private DataBaseConnector dbConnector;
-    private OrderRepository orderRepository;
+    @Test
+    void shouldSaveOrders() throws SQLException, IOException {
+        DataBaseConnector dbConnectorMock = mock(DataBaseConnector.class);
+        Connection connectionMock = mock(Connection.class);
+        PreparedStatement preparedStatementMock = mock(PreparedStatement.class);
+        Order order = new Order();
+        order.setUuid(UUID.randomUUID());
+        order.setId("1");
+        order.setRegion("Region");
+        order.setCountry("Country");
+        order.setItemType("ItemType");
+        order.setSalesChannel("Channel");
+        order.setPriority("High");
+        order.setDate("7/28/2012");
+        order.setShipDate("7/28/2012");
+        order.setUnitsSold(10L);
+        order.setUnitPrice(10.0);
+        order.setUnitCost(5.0);
+        order.setTotalRevenue(100.0);
+        order.setTotalCost(50.0);
+        order.setTotalProfit(50.);
+        order.setSelf("link");
+        when(dbConnectorMock.connectToDB()).thenReturn(connectionMock);
+        when(connectionMock.prepareStatement(anyString())).thenReturn(preparedStatementMock);
+        OrderRepository orderRepository = new OrderRepository(dbConnectorMock);
 
-    @BeforeEach
-    public void setUp() {
-        dbConnector = mock(DataBaseConnector.class);
-        orderRepository = new OrderRepository(dbConnector);
+        orderRepository.saveOrders(Collections.singletonList(order));
+
+        verify(preparedStatementMock, times(1)).addBatch();
+        verify(preparedStatementMock, times(1)).executeBatch();
     }
 
     @Test
-    public void shouldSaveOrders() throws SQLException, IOException {
-        Order order = new Order(1, "High", LocalDate.now(), "Region", "Country", "ItemType", "Channel", LocalDate.now(), 10, 10.0, 5.0, 100.0, 50.0, 50.0);
-        List<Order> orders = List.of(order);
-        Connection mockConnection = mock(Connection.class);
-        PreparedStatement mockPreparedStatement = mock(PreparedStatement.class);
-        when(dbConnector.connectToDB()).thenReturn(mockConnection);
-        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+    void shouldLoadOrders() throws SQLException, IOException {
+        DataBaseConnector dbConnectorMock = mock(DataBaseConnector.class);
+        Connection connectionMock = mock(Connection.class);
+        PreparedStatement preparedStatementMock = mock(PreparedStatement.class);
+        ResultSet resultSetMock = mock(ResultSet.class);
 
-        orderRepository.saveOrders(orders);
+        when(dbConnectorMock.connectToDB()).thenReturn(connectionMock);
+        when(connectionMock.prepareStatement(anyString())).thenReturn(preparedStatementMock);
+        when(preparedStatementMock.executeQuery()).thenReturn(resultSetMock);
+        when(resultSetMock.next()).thenReturn(true).thenReturn(false);
+        when(resultSetMock.getObject("uuid")).thenReturn(UUID.randomUUID());
+        when(resultSetMock.getString("id")).thenReturn("1");
+        when(resultSetMock.getString("region")).thenReturn("Region");
+        when(resultSetMock.getString("country")).thenReturn("country");
+        when(resultSetMock.getString("item_type")).thenReturn("item_type");
+        when(resultSetMock.getString("sales_channel")).thenReturn("sales_channel");
+        when(resultSetMock.getString("priority")).thenReturn("priority");
+        when(resultSetMock.getDate("date")).thenReturn(Date.valueOf("2012-07-12"));
+        when(resultSetMock.getDate("ship_date")).thenReturn(Date.valueOf("2012-07-12"));
+        when(resultSetMock.getLong("units_sold")).thenReturn(10L);
+        when(resultSetMock.getDouble("unit_price")).thenReturn(10.0);
+        when(resultSetMock.getDouble("unit_cost")).thenReturn(10.0);
+        when(resultSetMock.getDouble("total_revenue")).thenReturn(10.0);
+        when(resultSetMock.getDouble("total_cost")).thenReturn(10.0);
+        when(resultSetMock.getDouble("total_profit")).thenReturn(10.0);
+        when(resultSetMock.getString("self")).thenReturn("link");
 
-        verify(dbConnector).connectToDB();
-        verify(mockConnection).prepareStatement(anyString());
-        verify(mockPreparedStatement, times(orders.size())).addBatch();
-        verify(mockPreparedStatement).executeBatch();
-    }
-
-
-    @Test
-    public void loadAllOrdersTest() throws SQLException, IOException {
-        Connection mockConnection = mock(Connection.class);
-        Statement mockStatement = mock(Statement.class);
-        ResultSet mockResultSet = mock(ResultSet.class);
-        when(dbConnector.connectToDB()).thenReturn(mockConnection);
-        when(mockConnection.createStatement()).thenReturn(mockStatement);
-        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
-        when(mockResultSet.next()).thenReturn(true, false);
-        when(mockResultSet.getInt("order_id")).thenReturn(1, 2);
+        OrderRepository orderRepository = new OrderRepository(dbConnectorMock);
 
         List<Order> orders = orderRepository.loadAllOrders();
 
-        verify(dbConnector).connectToDB();
-        verify(mockConnection).createStatement();
-        verify(mockStatement).executeQuery(anyString());
-        verify(mockResultSet, times(2)).next();
         assertEquals(1, orders.size());
-        assertEquals(1, orders.get(0).getOrderId());
     }
+
 }
