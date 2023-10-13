f832cfe92da8d4621c3d1dde4f9788b15eeaf19a
wolfero <ferorock44@gmail.com>
2023-10-13 12:39:20 +0100

    :fire: remove all links logic

--- src/main/java/es/publicotech/repository/OrderRepository.java
+++ src/main/java/es/publicotech/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package es.publicotech.repository;
 
 import es.publicotech.database.DataBaseConnector;
-import es.publicotech.models.Links;
 import es.publicotech.models.Order;
 import es.publicotech.models.interfaces.OrderRepositoryInterface;
 import lombok.AllArgsConstructor;
@@ -14,7 +13,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @AllArgsConstructor
@@ -23,36 +21,27 @@ public class OrderRepository implements OrderRepositoryInterface {
 
     @Override
     public void saveOrders(List<Order> orders) throws SQLException, IOException {
-        String insertOrderSQL = "INSERT INTO ordersschema.orders (" +
+        String sql = "INSERT INTO ordersschema.orders (" +
                 "uuid, id, region, country, item_type, sales_channel, " +
                 "priority, date, ship_date, units_sold, unit_price, " +
-                "unit_cost, total_revenue, total_cost, total_profit) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
+                "unit_cost, total_revenue, total_cost, total_profit,self) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?) " +
                 "ON CONFLICT (uuid) DO UPDATE SET " +
                 "id = excluded.id, region = excluded.region, country = excluded.country, " +
                 "item_type = excluded.item_type, sales_channel = excluded.sales_channel, " +
                 "priority = excluded.priority, date = excluded.date, ship_date = excluded.ship_date, " +
                 "units_sold = excluded.units_sold, unit_price = excluded.unit_price, " +
                 "unit_cost = excluded.unit_cost, total_revenue = excluded.total_revenue, " +
-                "total_cost = excluded.total_cost, total_profit = excluded.total_profit;";
-
-        String linksSql = "INSERT INTO ordersschema.links (order_uuid, self) " +
-                "VALUES (?, ?) " +
-                "ON CONFLICT (order_uuid) DO UPDATE SET " +
-                "self = excluded.self;";
+                "total_cost = excluded.total_cost, total_profit = excluded.total_profit,self=excluded.self;";
         try (
                 Connection connection = dbConnector.connectToDB();
-                PreparedStatement ordersPreparedStatement = connection.prepareStatement(insertOrderSQL);
-                PreparedStatement linksPreparedStatement = connection.prepareStatement(linksSql)
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ) {
             for (Order order : orders) {
-                insertOrderValues(ordersPreparedStatement, order);
-                ordersPreparedStatement.addBatch();
-                insertLinkValues(linksPreparedStatement, order);
-                linksPreparedStatement.addBatch();
+                insertOrderValues(preparedStatement, order);
+                preparedStatement.addBatch();
             }
-            ordersPreparedStatement.executeBatch();
-            linksPreparedStatement.executeBatch();
+            preparedStatement.executeBatch();
         }
     }
 
@@ -73,11 +62,7 @@ public class OrderRepository implements OrderRepositoryInterface {
         preparedStatement.setDouble(13, order.getTotalRevenue());
         preparedStatement.setDouble(14, order.getTotalCost());
         preparedStatement.setDouble(15, order.getTotalProfit());
-    }
-
-    private void insertLinkValues(PreparedStatement preparedStatement, Order order) throws SQLException {
-        preparedStatement.setObject(1, order.getUuid());
-        preparedStatement.setString(2, order.getLinks().getSelf());
+        preparedStatement.setString(16, order.getSelf());
     }
 
     @Override
@@ -85,13 +70,10 @@ public class OrderRepository implements OrderRepositoryInterface {
         List<Order> orders;
         try (
                 Connection connection = dbConnector.connectToDB();
-                PreparedStatement ordersStatement = connection.prepareStatement("SELECT * FROM ordersschema.orders");
-                ResultSet ordersResultSet = ordersStatement.executeQuery();
-                PreparedStatement linksStatement = connection.prepareStatement("SELECT * FROM ordersschema.links");
-                ResultSet linksResultSet = linksStatement.executeQuery()
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM ordersschema.orders");
+                ResultSet resultSet = preparedStatement.executeQuery();
         ) {
-            orders = loadOrdersValues(ordersResultSet);
-            orders = loadLinksValues(linksResultSet, orders);
+            orders = loadOrdersValues(resultSet);
         }
         return orders;
     }
@@ -116,26 +98,10 @@ public class OrderRepository implements OrderRepositoryInterface {
             order.setTotalRevenue(resultSet.getDouble("total_revenue"));
             order.setTotalCost(resultSet.getDouble("total_cost"));
             order.setTotalProfit(resultSet.getDouble("total_profit"));
+            order.setSelf(resultSet.getString("self"));
             orders.add(order);
         }
         return orders;
     }
 
-
-    private List<Order> loadLinksValues(ResultSet resultSet, List<Order> orders) throws SQLException {
-        List<Order> newOrders = new ArrayList<>();
-        while (resultSet.next()) {
-            newOrders = orders.stream().peek(order -> {
-                try {
-                    if (order.getUuid() == resultSet.getObject("order_uuid")) {
-                        Links links = new Links(resultSet.getString("self"));
-                        order.setLinks(links);
-                    }
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }).collect(Collectors.toList());
-        }
-        return newOrders;
-    }
 }
